2e830036cc1d32e32dafdc25570d3d30285aaecf
Dr. Auxin <paul.torrillo@gmail.com>
2017-01-19 10:33:51 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        {
+            int index = 0;
+            while (index <= 9) {
+                System.out.println(fib(index));
+                index++;
+            }
+        }
+    }
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int term = fib(n - 1) + fib(n - 2);
+        return term;
+
     }
 }
+
+
