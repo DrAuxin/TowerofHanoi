f38155cb90b53499e61ac7519bda031ca98d9897
Paul <paul.torrillo@gmail.com>
2017-01-19 18:29:07 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,36 @@
 package com.company;
+import java.util.*;
 
 public class Main {
-
+    public static int x;
     public static void main(String[] args) {
         {
-            int index = 0;
-            while (index <= 9) {
-                System.out.println(fib(index));
-                index++;
+            x = 0;
+            Scanner user = new Scanner(System.in);
+            System.out.println("Enter the number of discs in your tower of Hanoi");
+            int n = user.nextInt();
+            int source = 1;
+            int spare = 2;
+            int target = 3;
+            tower(n, source, spare, target);
+                }
             }
-        }
-    }
-    public static int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        int term = fib(n - 1) + fib(n - 2);
-        return term;
 
+
+    public static void tower(int n, int source, int spare, int target) {
+  if (n <= 2)
+  {
+      System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + spare);
+      System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + target);
+      System.out.println("Step " + ++x +": Move the disc on tower " + spare + " to tower " + target);
     }
+    else
+  {
+      tower(n-1, source, target, spare);
+      System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + target);
+      tower(n-1, spare, source, target);
+  }
+}
 }
 
 
