2f776c98fb01accef05522c909760ffa4dbe6c8d
Paul <paul.torrillo@gmail.com>
2017-01-19 18:43:41 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,18 @@
 package com.company;
 import java.util.*;
-
-public class Main {
+public class Main
+{
     public static int x;
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         {
             x = 0;
             Scanner user = new Scanner(System.in);
-            System.out.println("Enter the number of discs in your tower of Hanoi");
+            System.out.println("Enter the number of discs in your tower of Hanoi.");
             int n = user.nextInt();
             if (n <= 1)
             {
-                String error = "This is not a tower of Hanoi";
+                String error = "This is not a tower of Hanoi.";
                 IllegalArgumentException e = new IllegalArgumentException(error);
                 throw e;
             }
@@ -19,24 +20,23 @@ public class Main {
             int spare = 2;
             int target = 3;
             tower(n, source, spare, target);
-                }
-            }
-
-
-    public static void tower(int n, int source, int spare, int target) {
-  if (n <= 2)
-  {
-      System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + spare);
-      System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + target);
-      System.out.println("Step " + ++x +": Move the disc on tower " + spare + " to tower " + target);
+        }
     }
-    else
-  {
-      tower(n-1, source, target, spare);
-      System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + target);
-      tower(n-1, spare, source, target);
-  }
-}
+    public static void tower(int n, int source, int spare, int target)
+    {
+        if (n <= 2)
+        {
+            System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + spare + ".");
+            System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + target + ".");
+            System.out.println("Step " + ++x +": Move the disc on tower " + spare + " to tower " + target + ".");
+        }
+        else
+        {
+            tower(n-1, source, target, spare);
+            System.out.println("Step " + ++x +": Move the disc on tower " + source + " to tower " + target + ".");
+            tower(n-1, spare, source, target);
+        }
+    }
 }
 
 
